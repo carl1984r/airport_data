ade99a4380ad958780a0ba8b23554e1f7b374394
Bry Cox <carl.cc@gmail.com>
2019-04-10 21:03:42 +0000

    Updated cli.rb

--- lib/airport_data/cli.rb
+++ lib/airport_data/cli.rb
@@ -3,6 +3,14 @@ class AirportData::CLI
 
   def call
     puts "Airport Data!"
+    list_airports
+  end
+
+  def list_airports
+    puts <<-DOC
+    1. F95 - Altha, FL - New Orleans
+    2. 0J6 - Headland, AL - New Orleans
+    DOC
   end
 
 end
