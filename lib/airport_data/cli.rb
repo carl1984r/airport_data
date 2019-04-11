7f0ff6fee116f9552b1e0e8336f04e702b77b69d
Bry Cox <carl.cc@gmail.com>
2019-04-11 19:50:31 +0000

    Updated CLI.

--- lib/airport_data/cli.rb
+++ lib/airport_data/cli.rb
@@ -2,15 +2,36 @@
 class AirportData::CLI
 
   def call
-    puts "Airport Data!"
     list_airports
+    menu
+    goodbye
   end
 
   def list_airports
+    puts "Airport Data!"
     puts <<-DOC
-    1. F95 - Altha, FL - New Orleans
-    2. 0J6 - Headland, AL - New Orleans
+    1. 0J6 - Headland, AL - New Orleans
+    2. ECP - Panama City, FL - New Orleans
     DOC
   end
 
+  def goodbye
+    puts "Tnanks for using airportdata."
+  end
+
+  def menu
+    puts "Enter the 3 letter airport identifier you would like for information on, otherwise type exit:"
+    input = nil
+    while input != "exit"
+    input = gets.strip
+    case input
+    when "0J6"
+      puts "0J6 - Headland, AL - New Orleans"
+    when "ECP"
+      puts "ECP - Panama City, FL - New Orleans"
+    when "list"
+      list_airports
+    end
+  end
+ end
 end
