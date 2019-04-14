dfefa87130cb3960205a2133fdab829a65566a3c
Bry Cox <carl.cc@gmail.com>
2019-04-14 14:59:49 +0000

    Updated scraper

--- lib/airport_data/scraper.rb
+++ lib/airport_data/scraper.rb
@@ -4,8 +4,12 @@ class AirportData::Scraper
   def self.scrape_airport_data
     arr = []
     doc = Nokogiri::HTML(open("https://www.faa.gov/airports/airport_safety/airportdata_5010/menu/contacts.cfm?Region=&District=&State=FL&County=&City=&Use=&Certification="))
-    doc.css("body").first
+    doc.css("body").first.css("table tbody tr td font")
     binding.pry
   end
+
+  #state: doc.css("body").first.css("table tbody tr td font")[0].text.split[0]
+  #region: doc.css("body").first.css("table tbody tr td font")[0].text.split[1]
+  #ado: doc.css("body").first.css("table tbody tr td font")[0].text.split[2]
 
 end
