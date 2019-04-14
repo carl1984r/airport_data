5096b32282c305914b6d8ab28d3ef390d90b3f13
Bry Cox <carl.cc@gmail.com>
2019-04-14 20:00:54 +0000

    Updated scraper x3

--- lib/airport_data/scraper.rb
+++ lib/airport_data/scraper.rb
@@ -4,28 +4,26 @@ class AirportData::Scraper
   def self.scrape_airport_data
     arr = []
     doc = Nokogiri::HTML(open("https://www.faa.gov/airports/airport_safety/airportdata_5010/menu/contacts.cfm?Region=&District=&State=FL&County=&City=&Use=&Certification="))
-    doc.css("body").first.css("table tbody tr td font")
+    #doc.css("body table")
     binding.pry
   end
 
-  #test: doc.css("body table")[1].css("tbody tr td font")[0].children[0].text
-  #test: doc.css("body table")[0].css("tbody tr td font")[2].children[0].text
+  #state: doc.css("body table")[1].css("tbody tr td font")[0].children[0].text
+  #region: doc.css("body table")[1].css("tbody tr td font")[0].children[3].children.text
+  #ado: doc.css("body table")[1].css("tbody tr td font")[0].children[6].text
+  #city: doc.css("body table")[1].css("tbody tr td font")[1].children[0].text
+  #county: doc.css("body table")[1].css("tbody tr td font")[1].children[2].text
+  #facility_name: doc.css("body table")[5].css("tbody tr td font")[2].children[0].text
+  #ownership_and_use: doc.css("body table")[1].css("tbody tr td font")[2].children[2].text
+  #location_id: doc.css("body table")[5].css("tbody tr td font")[3].children[0].children.text
+  #site_number: doc.css("body table")[5].css("tbody tr td font")[3].children[2].children.text
+  #airport_reference: doc.css("body table")[5].css("tbody tr td font")[4].children[0].children[0].text
+  #point_coordinates: doc.css("body table")[5].css("tbody tr td font")[4].children[0].children[2].text
+  #manager_name: doc.css("body table")[5].css("tbody tr td font")[5].children.text
+  #address_1: doc.css("body table")[5].css("tbody tr td font")[6].children[1].text
+  #address_2: doc.css("body table")[5].css("tbody tr td font")[6].children[4].children.text
+  #phone_1: doc.css("body table")[5].css("tbody tr td font")[9].children[2].text.strip
+  #phone_2: doc.css("body table")[5].css("tbody tr td font")[9].children[0].text.strip
 
-
-  #state: doc.css("body").first.css("table tbody tr td font")[0].text.split[0]
-  #region: doc.css("body").first.css("table tbody tr td font")[0].text.split[1]
-  #ado: doc.css("body").first.css("table tbody tr td font")[0].text.split[2]
-  #city: doc.css("body").first.css("table tbody tr td font")[1].children[0].text
-  #county: doc.css("body").first.css("table tbody tr td font")[1].children[2].text
-  #facility_name: doc.css("body").first.css("table tbody tr td font")[2].children[0].text
-  #ownership_and_use: doc.css("body").first.css("table tbody tr td font")[2].children[2].text
-  #location_id: doc.css("body").first.css("table tbody tr td font")[3].children[0].text
-  #site_number: doc.css("body").first.css("table tbody tr td font")[3].children[2].text
-  #airport_reference: doc.css("body").first.css("table tbody tr td font")[4].children[0].children[0].text
-  #point_coordinates: doc.css("body").first.css("table tbody tr td font")[4].children[0].children[2].text
-  #manager_name: doc.css("body").first.css("table tbody tr td font")[5].children.text
-  #address_1: doc.css("body").first.css("table tbody tr td font")[6].children[1].text
-  #address_2: doc.css("body").first.css("table tbody tr td font")[6].children[4].text
-  #phone: doc.css("body").first.css("table tbody tr td font")[9].children[2].text
 
 end
