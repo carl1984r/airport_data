edfb7e73ab4d1750462bcbc8ecda282dc3245418
Bry Cox <carl.cc@gmail.com>
2019-04-16 07:47:08 +0000

    Updated scraper x8

--- lib/airport_data/scraper.rb
+++ lib/airport_data/scraper.rb
@@ -25,21 +25,5 @@ class AirportData::Scraper
   end
 end
 
-  #state: doc.css("body table")[1].css("tbody tr td font")[0].children[0].text
-  #region: doc.css("body table")[1].css("tbody tr td font")[0].children[3].children.text
-  #airport_district_office: doc.css("body table")[1].css("tbody tr td font")[0].children[6].text
-  #city: doc.css("body table")[1].css("tbody tr td font")[1].children[0].text
-  #county: doc.css("body table")[1].css("tbody tr td font")[1].children[2].text
-  #facility_name: doc.css("body table")[5].css("tbody tr td font")[2].children[0].text
-  #ownership_and_use: doc.css("body table")[1].css("tbody tr td font")[2].children[2].text
-  #location_id: doc.css("body table")[5].css("tbody tr td font")[3].children[0].children.text
-  #site_number: doc.css("body table")[5].css("tbody tr td font")[3].children[2].children.text
-  #airport_reference: doc.css("body table")[5].css("tbody tr td font")[4].children[0].children[0].text
-  #point_coordinates: doc.css("body table")[5].css("tbody tr td font")[4].children[0].children[2].text
-  #manager_name: doc.css("body table")[5].css("tbody tr td font")[5].children.text
-  #address_1: doc.css("body table")[5].css("tbody tr td font")[6].children[1].text
-  #address_2: doc.css("body table")[5].css("tbody tr td font")[6].children[4].children.text
-  #phone_1: doc.css("body table")[5].css("tbody tr td font")[9].children[2].text.strip
-  #phone_2: doc.css("body table")[5].css("tbody tr td font")[9].children[0].text.strip
 
 
