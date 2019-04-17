ffe9d3250f443c60b9418760400370d2d44f03c9
Bry Cox <carl.cc@gmail.com>
2019-04-17 11:09:09 +0000

    Updated airports x3

--- lib/airport_data/airports.rb
+++ lib/airport_data/airports.rb
@@ -3,11 +3,36 @@ class AirportData::Airports
 
   attr_accessor :state, :region, :airport_district_office, :city, :facility_name, :location_id, :site_number, :airport_reference, :point_coordinates, :manager_name, :address, :phone_numbers
 
-  #def initialize(AirportData::Scraper.scrape_airport_data)
+  @@all = []
 
-  def self.create_from_scraper
-    v = AirportData::Scraper.scrape_airport_data
-      binding.pry
+  def initialize
+    @state = state
+    @region = region
+    @airport_district_office = airport_district_office
+    @city = city
+    @facility_name = facility_name
+    @location_id = location_id
+    @site_number = site_number
+    @airport_reference = airport_reference
+    @point_coordinates = point_coordinates
+    @manager_name = manager_name
+    @address = address
+    @phone_numbers = phone_numbers
+  end
+def self.create_from_scraper
+   v = AirportData::Scraper.scrape_airport_data
+   v.each do |x|
+     if x.any?
+       m = self.new
+       m.state = x.fetch(:state)
+       m.region = x.fetch(:region)
+       m.airport_district_office = x.fetch(:airport_district_office)
+       m.city = x.fetch(:city)
+       m.facility_name = x.fetch(:facility_name)
+     binding.pry
+     end
+   end
+
     #Return instances of updated airport information.
     #puts <<-DOC
     #1. 0J6 - Headland, AL - New Orleans
@@ -19,5 +44,4 @@ class AirportData::Airports
   #  airport
 
   end
-
 end
