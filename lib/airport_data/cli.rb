0f8878dc35f7022e84e62a4485e83d43204eaa0d
Bry Cox <carl.cc@gmail.com>
2019-04-18 09:19:03 +0000

    Updated CLI x2

--- lib/airport_data/cli.rb
+++ lib/airport_data/cli.rb
@@ -2,36 +2,38 @@
 class AirportData::CLI
 
   def call
-    list_airports
     menu
     goodbye
   end
 
   def list_airports
-    puts "Airport Data!"
     AirportData::Airports.create_from_scraper
-
+    AirportData::Airports.all
   end
 
   def goodbye
-    puts "Tnanks for using airportdata."
+    #puts "Tnanks for using airportdata."
   end
 
   def menu
     input = nil
     while input != "exit"
-    puts "Enter the 3 letter airport identifier you would like for information on, otherwise type exit:"
-    input = gets.strip
-    case input
-    when "0J6"
-      puts "0J6 - Headland, AL - New Orleans"
-    when "ECP"
-      puts "ECP - Panama City, FL - New Orleans"
-    when "list"
-      list_airports
-    else
-      puts "Entry not in database. Please retry."
+    puts "-----------------------Airport Data Menu-----------------------"
+    puts "To search airport by location identifier: type 'location_id'"
+    puts "To search airports by city: type 'city'"
+    puts "To search airports by state: type 'state'"
+    puts "To exit program - type 'exit'"
+
+  #  input = gets.strip
+  #  case input
+  #    when "location_id"
+  #    puts "Enter the location identifier.  Examples: F95, AAF, ECP"
+  #      input_1 = gets.strip
+  #      list_airports.map {|x| x if x.location_id == "input_1"}
+  #    end
+
     end
+   end
   end
  end
 end
