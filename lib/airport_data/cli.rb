a007cdccc204667a3df7486a78b0be7f05443f16
Bry Cox <carl.cc@gmail.com>
2019-04-19 17:37:38 +0000

    Updated CLI x5

--- lib/airport_data/cli.rb
+++ lib/airport_data/cli.rb
@@ -9,9 +9,30 @@ class AirportData::CLI
   end
 
   def welcome
+    puts ""
+    puts "==============================================================="
     puts "Welcome to Airport Data!  Please wait for the program to update"
     puts "with the latest airport information.  This should take less than"
     puts "a minute..."
+    puts "==============================================================="
+    puts ""
+  end
+
+  def display(x)
+    puts ""
+    puts "-----------------------------------------------------------"
+    puts "Facility Name:  #{x.facility_name}"
+    puts "Address:  #{x.address}"
+    puts "Manager Name:  #{x.manager_name}"
+    puts "Phone Number(s):  #{x.phone_numbers.flatten[0]}  #{x.phone_numbers.flatten[1]}"
+    puts "Location ID:  #{x.location_id}"
+    puts "Airport District Office:  #{x.airport_district_office}"
+    puts "Airport Reference:  #{x.airport_reference}"
+    puts "Point Coordinates:  #{x.point_coordinates}"
+    puts "Region:  #{x.region}"
+    puts "Site Number:  #{x.site_number}"
+    puts "-----------------------------------------------------------"
+    puts ""
   end
 
   def build_airports
@@ -25,32 +46,35 @@ class AirportData::CLI
   def menu
     input = nil
     while input != "exit"
+    puts ""
     puts "-----------------------Airport Data Menu-----------------------"
     puts "To search airport by location identifier: type 'location_id'"
     puts "To search airports by city: type 'city'"
     puts "To search airports by state: type 'state'"
     puts "To exit program - type 'exit'"
+    puts "---------------------------------------------------------------"
+    puts ""
     input = gets.strip
       case input
         when "location_id"
-        puts "Enter the location identifier.  Examples: F95, AAF, ECP"
+           puts ""
+           puts "---------------------------------------------------------------"
+           puts "Enter the location identifier.  Examples: F95, AAF, ECP"
+           puts "---------------------------------------------------------------"
+           puts ""
            input_1 = gets.strip
-           v = AirportData::Airports.all.select {|x| x if x.location_id == input_1}.map{|y| y}
-           v.each do |x|
-             puts "-----------------------------------------------------------"
-             puts "Facility Name:  #{x.facility_name}"
-             puts "Address:  #{x.address}"
-             puts "Manager Name:  #{x.manager_name}"
-             puts "Phone Number(s):  #{x.phone_numbers.flatten[0]}  #{x.phone_numbers.flatten[1]}"
-             puts "Location ID:  #{x.location_id}"
-             puts "Airport District Office:  #{x.airport_district_office}"
-             puts "Airport Reference:  #{x.airport_reference}"
-             puts "Point Coordinates:  #{x.point_coordinates}"
-             puts "Region:  #{x.region}"
-             puts "Site Number:  #{x.site_number}"
-             puts "-----------------------------------------------------------"
-           end
-        end
-     end
+           v = AirportData::Airports.all.select {|x| x if x.location_id == input_1.upcase}.map{|y| y}
+           v.each {|x| display(x)}
+        when "city"
+           puts ""
+           puts "---------------------------------------------------------------"
+           puts "Enter the name of the city the airport is located in."
+           puts "---------------------------------------------------------------"
+           puts ""
+           input_2 = gets.strip
+           v = AirportData::Airports.all.select {|x| x if x.city == input_2.upcase}.map{|y| y}
+           v.each {|x| display(x)}
+      end
+    end
   end
 end
