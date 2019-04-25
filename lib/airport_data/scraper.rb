9742c0315472ec5745c247f81fd21a41a44750ea
Bry Cox <carl.cc@gmail.com>
2019-04-25 18:51:51 +0000

    Cleaned up scraper X1

--- lib/airport_data/scraper.rb
+++ lib/airport_data/scraper.rb
@@ -10,15 +10,15 @@ class AirportData::Scraper
       index[:state] = x.css("tbody tr td font")[0].children[0].text if x.css("tbody tr td font").any?
       index[:region] = x.css("tbody tr td font")[0].children[3].children.text if x.css("tbody tr td font").any? && x.css("tbody tr td font")[0].children[3].respond_to?(:'any?')
       index[:airport_district_office] = x.css("tbody tr td font")[0].children[6].text if x.css("tbody tr td font").any? && x.css("tbody tr td font")[0].children[6].respond_to?(:'any?')
-      index[:city] = x.css("tbody tr td font")[1].children[0].text if x.css("tbody tr td font").any? && x.css("tbody tr td font")[1].respond_to?(:'any?')
-      index[:facility_name] = x.css("tbody tr td font")[2].children[0].text if x.css("tbody tr td font").any? && x.css("tbody tr td font")[2].respond_to?(:'any?')
-      index[:location_id] = x.css("tbody tr td font")[3].children[0].children.text if x.css("tbody tr td font").any? && x.css("tbody tr td font")[3].respond_to?(:'any?')
-      index[:site_number] = x.css("tbody tr td font")[3].children[2].children.text if x.css("tbody tr td font").any? && x.css("tbody tr td font")[3].respond_to?(:'any?')
-      index[:airport_reference] = x.css("tbody tr td font")[4].children[0].children[0].text if x.css("tbody tr td font").any? && x.css("tbody tr td font")[4].respond_to?(:'any?')
-      index[:point_coordinates] = x.css("tbody tr td font")[4].children[0].children[2].text if x.css("tbody tr td font").any? && x.css("tbody tr td font")[4].respond_to?(:'any?')
+      index[:city] = x.css("tbody tr td font")[1].children[0].text if x.css("tbody tr td font")[1].respond_to?(:'any?')
+      index[:facility_name] = x.css("tbody tr td font")[2].children[0].text if x.css("tbody tr td font")[2].respond_to?(:'any?')
+      index[:location_id] = x.css("tbody tr td font")[3].children[0].children.text if x.css("tbody tr td font")[3].respond_to?(:'any?')
+      index[:site_number] = x.css("tbody tr td font")[3].children[2].children.text if x.css("tbody tr td font")[3].respond_to?(:'any?')
+      index[:airport_reference] = x.css("tbody tr td font")[4].children[0].children[0].text if x.css("tbody tr td font")[4].respond_to?(:'any?')
+      index[:point_coordinates] = x.css("tbody tr td font")[4].children[0].children[2].text if x.css("tbody tr td font")[4].respond_to?(:'any?')
       index[:manager_name] = x.css("tbody tr td font")[5].children.text if x.css("tbody tr td font")[5].respond_to?(:'any?')
       index[:address] = x.css("tbody tr td font")[6].text.gsub("\r\n        ", " - ") if x.css("tbody tr td font")[6].respond_to?(:'any?')
-      index[:phone_numbers] = x.css("tbody tr td font")[9].text.scan(/(\(?\d{3}\)?\s?\-?\d{3}.{5})/).uniq  if x.css("tbody tr td font")[9].respond_to?(:'any?') && x.css("tbody tr td font").any?
+      index[:phone_numbers] = x.css("tbody tr td font")[9].text.scan(/(\(?\d{3}\)?\s?\-?\d{3}.{5})/).uniq  if x.css("tbody tr td font")[9].respond_to?(:'any?')
       arr.push(index)
     end
   arr
